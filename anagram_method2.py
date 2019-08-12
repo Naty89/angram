4e243286c39ed2881bf55531e9e54d2e31b2a30c
Naty89 <37753984+Naty89@users.noreply.github.com>
2019-08-11 20:16:06 -0400

    Update anagram_method2.py

--- anagram_method2.py
+++ anagram_method2.py
@@ -1,18 +1,4 @@
 def anagram(x, y):
-	# l = []
-	# o = "".join(y)
-	# p = x.replace(" ", "")
-	# e = p.lower()
-	# if len(e) == len(o):
-	# 	for i in o:
-	# 		if i in e:
-	# 			l.append(True)
-	# 		else:
-	# 			l.append(False)
-	# else:	
-	# 	return False
-
-	# return all(l)
 
 	return sorted(x.replace(" ","").lower()) == sorted("".join(y))
 
